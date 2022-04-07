59ea8c80a2feb0a23e80bcb78303babf96e5c47b
stevenglasford <lr4fzuk0@anonaddy.me>
2022-04-07 08:56:00 -0400

    Add basic script for allowing the grow tent to cycle through pumps

---
+++ grow.py
@@ -0,0 +1,49 @@
+#! /usr/bin/python
+
+###################
+###   Imports   ###
+###################
+import sys
+import time
+import datetime
+import subprocess
+
+##########################
+###  Control variables ###
+##########################
+# Next iteration should be able to control more things
+#humidity
+#temperature
+#EC
+#PH
+#max_water_level
+#current_bucket_ID
+pumping_time = 300 #allows the pump to run for 5 minutes
+
+#########################
+###  Board variables  ###
+#########################
+# need to change to be more generalized it should be able to just plug and play
+# currently the ports and serial numbers for the boards are hardcoded.
+on  =  ['ykushcmd -s YK24039 -u 1',
+        'ykushcmd -s YK24039 -u 2',
+        'ykushcmd -s YK24041 -u 1',
+        'ykushcmd -s YK24041 -u 2']
+off =  ['ykushcmd -s YK24039 -d 1',
+        'ykushcmd -s YK24039 -d 2',
+        'ykushcmd -s YK24041 -d 1',
+        'ykushcmd -s YK24041 -d 2']
+
+# Turn off all buckets at startup
+subprocess.call('./off.sh', shell=True)
+
+# Cycle through the buckets turning them on then sleeping
+# then turning off, then go to next bucket. Simple.
+while True: # Enter infinite loop
+	for bucket in range(len(on)): # Enter bucket cycle
+		# Turn on the buckets in order
+		subprocess.call(on[bucket])
+		# Sleep for a predetermined amount of time
+		time.sleep(pumping_time)
+		# Turn off bucket after the predetermined amount of time
+		subprocess.call(off[bucket])
