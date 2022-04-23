1eca740c754cf569c531de94f3ed12f6876b5efd
stevenglasford <lr4fzuk0@anonaddy.me>
2022-04-23 18:43:21 -0400

    Add a new cron file to allow for more complicated boot-up sequence

---
+++ cron.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+# This file is for your cron tab
+
+# This shell file should be added to your cron.d `crontab -e`
+# It should look like the following at the end of the file
+	# `@reboot ./home/pi/hemp/off.sh`
+
+
