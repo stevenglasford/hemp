a3d2ad2c1945f8a7461c5608aa07e20856809996
stevenglasford <lr4fzuk0@anonaddy.me>
2022-04-23 18:48:28 -0400

    Remove grow.py and add basic functionality to show sign of life when booting up in cron file.

--- cron.sh
+++ cron.sh
@@ -6,4 +6,9 @@
 # It should look like the following at the end of the file
 	# `@reboot ./home/pi/hemp/off.sh`
 
-
+# Turn off system
+./off.sh;
+# sleep for a second
+sleep 5s;
+# Turn on system
+python on.py
